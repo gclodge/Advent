8462b2dbc8169a1f56d2b893e0422badbccb65ae
Graham Clodge <graham.clodge@gmail.com>
2022-12-02 11:21:16 -0500

    Finished 2022.02

---
+++ src/Advent.2022/Days/Day.02.cs
@@ -0,0 +1,123 @@
+﻿namespace Advent._2022;
+
+//< A, X -> Rock
+//< B, Y -> Paper
+//< C, Z -> Scissors
+
+//< Second column unknown, delim in space
+
+enum RpsMove
+{
+    Rock,
+    Paper,
+    Scissors
+}
+
+public class RockPaperScissors
+{
+    public const char Delim = ' ';
+    private readonly List<string> _input;
+
+    public RockPaperScissors(IEnumerable<string> input)
+    {
+        _input = input.ToList();
+    }
+
+    public long SolvePartOne()
+    {
+        return _input.Select(PlayPartOne).Sum();
+    }
+
+    public long SolvePartTwo()
+    {
+        return _input.Select(PlayPartTwo).Sum();
+    }
+
+    static long PlayPartOne(string line)
+    {
+        var moves = line.Split(Delim).Select(ParsePartOne).ToList();
+
+        RpsMove mine = moves[1];
+        RpsMove theirs = moves[0];
+
+        return GetScore(mine, theirs);
+    }
+
+    static long PlayPartTwo(string line)
+    {
+        //< X lose, Y draw, Z win
+        var cmds = line.Split(Delim);
+
+        RpsMove theirs = ParsePartOne(cmds[0]);
+        RpsMove mine = ParsePartTwo(cmds[1], theirs);
+
+        return GetScore(mine, theirs);
+    }
+
+    static long GetScore(RpsMove mine, RpsMove theirs)
+    {
+        long resScore = GetResultScore(mine, theirs);
+        long moveScore = GetMoveScore(mine);
+
+        return resScore + moveScore;
+    }
+
+    static RpsMove ParsePartOne(string val)
+    {
+        return val.ToLower() switch
+        {
+            "a" => RpsMove.Rock,
+            "x" => RpsMove.Rock,
+            "b" => RpsMove.Paper,
+            "y" => RpsMove.Paper,
+            "c" => RpsMove.Scissors,
+            "z" => RpsMove.Scissors,
+            _ => throw new ArgumentException($"What does {val} even mean, man?")
+        };
+    }
+
+    static RpsMove ParsePartTwo(string val, RpsMove theirs)
+    {
+        return val.ToLower() switch
+        {
+            "x" => GetCorrespondingMove(theirs, theyWin: true),
+            "y" => theirs,
+            "z" => GetCorrespondingMove(theirs, theyWin: false),
+            _ => throw new ArgumentException($"What does {theirs} even mean, man?")
+        };
+    }
+
+    static RpsMove GetCorrespondingMove(RpsMove theirs, bool theyWin)
+    {
+        if (theirs == RpsMove.Rock) return theyWin ? RpsMove.Scissors : RpsMove.Paper;
+        if (theirs == RpsMove.Paper) return theyWin ? RpsMove.Rock : RpsMove.Scissors;
+        if (theirs == RpsMove.Scissors) return theyWin ? RpsMove.Paper : RpsMove.Rock;
+
+        throw new Exception($"How'd you even get here, my dude?");
+    }
+
+    const long WinScore = 6;
+    const long DrawScore = 3;
+    const long LossScore = 0;
+    static long GetResultScore(RpsMove mine, RpsMove theirs)
+    {
+        if (mine == theirs) return DrawScore;
+
+        if (mine == RpsMove.Rock && theirs == RpsMove.Scissors) return WinScore;
+        if (mine == RpsMove.Paper && theirs == RpsMove.Rock) return WinScore;
+        if (mine == RpsMove.Scissors && theirs == RpsMove.Paper) return WinScore;
+
+        return LossScore;
+    }
+
+    static long GetMoveScore(RpsMove move)
+    {
+        return move switch
+        {
+            RpsMove.Rock => 1,
+            RpsMove.Paper => 2,
+            RpsMove.Scissors => 3,
+            _ => throw new ArgumentException($"What does {move} even mean, man?")
+        };
+    }
+}
---
+++ tests/Advent.Tests/2022/Day.02.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using Xunit;
+using Assert = Xunit.Assert;
+
+using Advent._2022;
+
+namespace Advent.Tests._2022;
+
+public class Day02 : IDailyTest
+{
+    public int Number => 2;
+    public int Year => 2022;
+
+    public string Input => TestHelper.GetInputFile(this);
+
+    [Fact]
+    public void Test_KnownInputs()
+    {
+        var lines = new List<string>()
+        {
+            "A Y",
+            "B X",
+            "C Z"
+        };
+
+        var rps = new RockPaperScissors(lines);
+        long scoreP1 = rps.SolvePartOne();
+        long scoreP2 = rps.SolvePartTwo();
+        long ExpectedP1 = 15;
+        long ExpectedP2 = 12;
+
+        Assert.Equal(ExpectedP1, scoreP1);
+        Assert.Equal(ExpectedP2, scoreP2);
+    }
+
+
+    [Fact]
+    public void PartOne()
+    {
+        var input = Input.Parse();
+
+        var rps = new RockPaperScissors(input);
+        long score = rps.SolvePartOne();
+        long Expected = 13526;
+
+        Assert.Equal(Expected, score);
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        var input = Input.Parse();
+
+        var rps = new RockPaperScissors(input);
+        long score = rps.SolvePartTwo();
+        long Expected = 14204;
+
+        Assert.Equal(Expected, score);
+    }
+}
