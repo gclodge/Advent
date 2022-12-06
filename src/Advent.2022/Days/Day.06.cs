861f353fba71a9cd00156c1f0115d9945aa06adf
Graham Clodge <graham.clodge@gmail.com>
2022-12-06 10:29:50 -0500

    Finished 2022.06

---
+++ src/Advent.2022/Days/Day.06.cs
@@ -0,0 +1,32 @@
+﻿namespace Advent._2022;
+
+public class DatastreamBuffer
+{
+    private readonly string _input;
+
+    public DatastreamBuffer(string input)
+    {
+        _input = input;
+    }
+
+    public int GetFirstStartOfPacketMarker(int markerSize)
+    {
+        var q = new Queue<char>();
+        foreach (int i in Enumerable.Range(0, _input.Length))
+        {
+            if (q.Count != markerSize)
+            {
+                q.Enqueue(_input[i]);
+                continue;
+            }
+
+            if (q.Distinct().Count() == markerSize) return i;
+
+            q.Dequeue();
+            q.Enqueue(_input[i]);
+        }
+
+        return -1;
+    }
+
+}
---
+++ tests/Advent.Tests/2022/Day.06.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using Xunit;
+using Assert = Xunit.Assert;
+
+using Advent._2022;
+
+namespace Advent.Tests._2022;
+
+public class Day06 : IDailyTest
+{
+    public int Number => 6;
+    public int Year => 2022;
+
+    public string Input => TestHelper.GetInputFile(this);
+
+    [Fact]
+    public void Test_KnownInputs()
+    {
+        var lines = new List<(string str, int idxA, int idxB)>()
+        {
+            ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 19),
+            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 23),
+            ("nppdvjthqldpwncqszvftbrmjlhg", 6, 23),
+            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 29),
+            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 26)
+        };
+
+        foreach (var line in lines)
+        {
+            var buff = new DatastreamBuffer(line.str);
+            int idxA = buff.GetFirstStartOfPacketMarker(4);
+            int idxB = buff.GetFirstStartOfPacketMarker(14);
+
+            Assert.Equal(line.idxA, idxA);
+            Assert.Equal(line.idxB, idxB);
+        }
+    }
+
+    [Fact]
+    public void PartOne()
+    {
+        var input = Input.Parse().Single();
+
+        var buff = new DatastreamBuffer(input);
+        int ActualIdx = buff.GetFirstStartOfPacketMarker(4);
+        int ExpectedIdx = 1850;
+
+        Assert.Equal(ExpectedIdx, ActualIdx);
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        var input = Input.Parse().Single();
+
+        var buff = new DatastreamBuffer(input);
+        int ActualIdx = buff.GetFirstStartOfPacketMarker(14);
+        int ExpectedIdx = 2823;
+
+        Assert.Equal(ExpectedIdx, ActualIdx);
+    }
+}
