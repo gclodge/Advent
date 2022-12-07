1612ce0da4a8b7be2991830486964e25c17bba98
Graham Clodge <graham.clodge@gmail.com>
2022-12-07 14:36:22 -0500

    Finished 2022.07

---
+++ src/Advent.2022/Days/Day.07.cs
@@ -0,0 +1,144 @@
+﻿using Advent.Domain;
+
+namespace Advent._2022;
+
+class File
+{
+    public string Name { get; private set; } = string.Empty;
+    public string Directory { get; private set; } = string.Empty;
+
+    public string Path => Directory + "/" + Name;
+
+    public long Size { get; private set; }
+
+    public File(string dir, string line)
+    {
+        Directory = dir;
+
+        var vals = line.Split(' ');
+        Size = long.Parse(vals[0]);
+        Name = vals[1];
+    }
+}
+
+class Command
+{
+    public const string Prefix = "$";
+
+    public string Type { get; private set; } = string.Empty;
+    public string Arg { get; private set; } = string.Empty;
+
+    public Command(string line)
+    {
+        var vals = line.Split(' ');
+        Type = vals[1];
+        if (vals.Length > 2) Arg = vals[2];
+    }
+}
+
+public class FileSystemBrowser
+{
+    const char DELIM = '/';
+
+    private readonly ICollection<string> _input;
+
+    private readonly ICollection<File> _files = new List<File>();
+
+    private readonly IDictionary<string, long> _dirs = new Dictionary<string, long>();
+
+    private readonly Stack<string> Path = new();
+
+    public long UsedSpace => _dirs["/"];
+
+    public FileSystemBrowser(IEnumerable<string> inputs)
+    {
+        _input = inputs.ToList();
+
+        ParseFilesFromInputs();
+        ParseDirectoryMap();
+    }
+
+    void ParseFilesFromInputs()
+    {
+        foreach (int i in Enumerable.Range(0, _input.Count))
+        {
+            string line = _input.ElementAt(i);
+
+            if (line.StartsWith(Command.Prefix))
+            {
+                HandleCommand(line);
+                continue;
+            }
+
+            if (line.StartsWith("dir")) continue;
+
+            var file = new File(GetPath(Path), line);
+            _files.Add(file);
+        }
+    }
+
+    void HandleCommand(string line)
+    {
+        var cmd = new Command(line);
+        if (cmd.Type == "cd")
+        {
+            if (cmd.Arg == "..") Path.Pop();
+            else Path.Push(cmd.Arg);
+        }
+    }
+
+    void ParseDirectoryMap()
+    {
+        foreach (var file in _files)
+        {
+            if (!_dirs.ContainsKey(file.Directory)) _dirs[file.Directory] = 0;
+
+            if (file.Directory == "/")
+            {
+                AddToDir(file.Directory, file.Size);
+                continue;
+            }
+
+            var parts = file.Directory.Split(DELIM);
+            string path = string.Empty;
+
+            foreach (var part in parts)
+            {
+                path += path.EndsWith(DELIM) ? part : $"{DELIM}{part}";
+
+                AddToDir(path, file.Size);
+            }
+        }
+    }
+
+    void AddToDir(string path, long size)
+    {
+        if (!_dirs.ContainsKey(path)) _dirs[path] = 0;
+
+        _dirs[path] += size;
+    }
+
+    public long CalculatePartOne(int maxSize = 100000)
+    {
+        var below = _dirs.Where(kvp => kvp.Value <= maxSize).ToList();
+        long total = below.Select(x => x.Value).Sum();
+        return total;
+    }
+
+    public long CalculatePartTwo(long totalSpace = 70000000, long targetSpace = 30000000)
+    {
+        long unused = totalSpace - UsedSpace;
+
+        var candidates = _dirs.Where(kvp => unused + kvp.Value > targetSpace).ToList();
+        if (candidates.Count == 0) throw new Exception($"No valid candidate directories found!");
+
+        var smallest = candidates.OrderBy(x => x.Value).First();
+
+        return smallest.Value;
+    }
+
+    static string GetPath(Stack<string> currPath)
+    {
+        return string.Join(DELIM, currPath.Reverse()).Replace("//", "/");
+    }
+}
---
+++ tests/Advent.Tests/2022/Day.07.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using Xunit;
+using Assert = Xunit.Assert;
+
+using Advent._2022;
+
+namespace Advent.Tests._2022;
+
+public class Day07 : IDailyTest
+{
+    public int Number => 7;
+    public int Year => 2022;
+
+    public string Input => TestHelper.GetInputFile(this);
+
+    [Fact]
+    public void Test_KnownInputs()
+    {
+        var lines = new List<string>()
+        {
+            "$ cd /",
+            "$ ls",
+            "dir a",
+            "14848514 b.txt",
+            "8504156 c.dat",
+            "dir d",
+            "$ cd a",
+            "$ ls",
+            "dir e",
+            "29116 f",
+            "2557 g",
+            "62596 h.lst",
+            "$ cd e",
+            "$ ls",
+            "584 i",
+            "$ cd ..",
+            "$ cd ..",
+            "$ cd d",
+            "$ ls",
+            "4060174 j",
+            "8033020 d.log",
+            "5626152 d.ext",
+            "7214296 k"
+        };
+
+        var browser = new FileSystemBrowser(lines);
+        long p1 = browser.CalculatePartOne();
+        long p2 = browser.CalculatePartTwo();
+
+        long ExpectedP1 = 95437;
+        long ExpectedP2 = 24933642;
+        Assert.Equal(ExpectedP1, p1);
+        Assert.Equal(ExpectedP2, p2);
+    }
+
+    [Fact]
+    public void PartOne()
+    {
+        var input = Input.Parse();
+
+        var browser = new FileSystemBrowser(input);
+        long p1 = browser.CalculatePartOne();
+
+        long ExpectedP1 = 1477771;
+        Assert.Equal(ExpectedP1, p1);
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        var input = Input.Parse();
+
+        var browser = new FileSystemBrowser(input);
+        long p2 = browser.CalculatePartTwo();
+
+        long ExpectedP2 = 3579501;
+        Assert.Equal(ExpectedP2, p2);
+    }
+}
