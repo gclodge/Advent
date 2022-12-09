4097141577a923a5d159794374c776fed7121655
Graham Clodge <graham.clodge@gmail.com>
2022-12-09 10:56:20 -0500

    Finished 2022.09

---
+++ src/Advent.2022/Days/Day.09.cs
@@ -0,0 +1,86 @@
+﻿namespace Advent._2022;
+
+internal record Position(int X, int Y)
+{
+    internal Position Move(Movement move)
+    {
+        return move.Direction switch
+        {
+            "R" => new Position(X + 1, Y),
+            "L" => new Position(X - 1, Y),
+            "U" => new Position(X, Y + 1),
+            "D" => new Position(X, Y - 1),
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    internal Position MoveToHead(Position head)
+    {
+        var d = (head.X - X, head.Y - Y);
+
+        return d switch
+        {
+            (2, 2)   => new Position(X + 1, Y + 1),
+            (-2, -2) => new Position(X - 1, Y - 1),
+            (-2, 2)  => new Position(X - 1, Y + 1),
+            (2, -2)  => new Position(X + 1, Y - 1),
+            (var x, -2) => new Position(X + x, Y - 1),
+            (var x, 2) => new Position(X + x, Y + 1),
+            (-2, var y) => new Position(X - 1, Y + y),
+            (2, var y) => new Position(X + 1, Y + y),
+            _ => this
+        };
+    }
+}
+
+internal record Movement(string Direction, int Magnitude)
+{
+    internal static Movement Parse(string line)
+    {
+        var vals = line.Split(' ');
+        return new Movement(vals[0], int.Parse(vals[1]));
+    }
+}
+
+public class Rope
+{
+    private readonly ICollection<string> _input;
+    private readonly ICollection<Movement> _movements;
+
+    private readonly HashSet<Position> _tailPos = new();
+
+    private Position _head = new(0, 0);
+    private Position[] _tails = Array.Empty<Position>();
+
+    public int UniqueTailPositions => _tailPos.Count;
+
+    public Rope(IEnumerable<string> input)
+    {
+        _input = input.ToList();
+        _movements = _input.Select(x => Movement.Parse(x)).ToList();
+    }
+
+    public Rope ApplyMovements(int ropeSize = 2)
+    {
+        _tailPos.Add(_head);
+        _tails = Enumerable.Range(0, ropeSize - 1).Select(_ => new Position(0, 0)).ToArray();
+
+        foreach (var movement in _movements)
+        {
+            foreach (int _ in Enumerable.Range(0, movement.Magnitude))
+            {
+                _head = _head.Move(movement);
+
+                foreach (int i in Enumerable.Range(0, _tails.Length))
+                {
+                    Position head = i == 0 ? _head : _tails[i - 1];
+                    _tails[i] = _tails[i].MoveToHead(head);
+                }
+
+                _tailPos.Add(_tails.Last());
+            }
+        }
+
+        return this;
+    }
+}
---
+++ tests/Advent.Tests/2022/Day.09.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+using Xunit;
+using Assert = Xunit.Assert;
+
+using Advent._2022;
+
+namespace Advent.Tests._2022;
+
+public class Day09 : IDailyTest
+{
+    public int Number => 9;
+    public int Year => 2022;
+
+    public string Input => TestHelper.GetInputFile(this);
+
+    [Fact]
+    public void Test_KnownInputs()
+    {
+        var linesA = new List<string>()
+        {
+            "R 4",
+            "U 4",
+            "L 3",
+            "D 1",
+            "R 4",
+            "D 1",
+            "L 5",
+            "R 2"
+        };
+
+        var linesB = new List<string>()
+        {
+            "R 5",
+            "U 8",
+            "L 8",
+            "D 3",
+            "R 17",
+            "D 10",
+            "L 25",
+            "U 20"
+        };
+
+        int ExpectedTailPositionsA = 13;
+        int ExpectedTailPositionsB = 1;
+        int ExpectedTailPositionsC = 36;
+
+        var ropeA = new Rope(linesA).ApplyMovements();
+        var ropeB = new Rope(linesA).ApplyMovements(10);
+        var ropeC = new Rope(linesB).ApplyMovements(10);
+
+        int countA = ropeA.UniqueTailPositions;
+        int countB = ropeB.UniqueTailPositions;
+        int countC = ropeC.UniqueTailPositions;
+
+        Assert.Equal(ExpectedTailPositionsA, countA);
+        Assert.Equal(ExpectedTailPositionsB, countB);
+        Assert.Equal(ExpectedTailPositionsC, countC);
+    }
+
+    [Fact]
+    public void PartOne()
+    {
+        var input = Input.Parse();
+
+        int ExpectedTailPositions = 6354;
+        var rope = new Rope(input).ApplyMovements();
+        int count = rope.UniqueTailPositions;
+
+        Assert.Equal(ExpectedTailPositions, count);
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        var input = Input.Parse();
+
+        int ExpectedTailPositions = 2651;
+        var rope = new Rope(input).ApplyMovements(10);
+        int count = rope.UniqueTailPositions;
+
+        Assert.Equal(ExpectedTailPositions, count);
+    }
+}
